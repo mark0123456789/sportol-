6aba4b3e35306eb8dd368ce2be9ea05a95f1fda3
PiringerMark <Piringerm@kkszki.hu>
2026-09-23 10:57:00 +0200

    get all results ready

--- sportoló/Controllers/EredmenyController.cs
+++ sportoló/Controllers/EredmenyController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
 using sportoló.Models;
 
 namespace sportoló.Controllers
@@ -9,10 +10,33 @@ namespace sportoló.Controllers
     [ApiController]
     public class EredmenyController : ControllerBase
     {
-        private readonly string connectionstring = "Server = localhost; Database=sportolo13b;uid=root;Password=;" ;
+        private readonly string Connectionstring = "Server = localhost; Database=sportolo13b;uid=root;Password=;";
 
         [HttpGet]
         //összes eredmény listázása
+        public List<eredmeny> GetAllResoults()
+        {
+            var connector = new MySqlConnection(Connectionstring);
+            connector.Open();
+            string sql = "SELECT * FROM `eredmeny`";
+            var cmd = new MySqlCommand(sql, connector);
+            var dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                var eredmeny = new eredmeny
+                {
+                    id = dataReader.GetInt32(0),
+                    Competition = dataReader.GetString(1),
+                    Description = dataReader.GetString(2),
+                    ResultTime = dataReader.GetDateTime(3),
+                    UpdateTime = dataReader.GetDateTime(4),
+                    SportoloId = dataReader.GetInt32(5)
+                };
+
+            }
+            connector.Close();
+            return null;
+        }
         [HttpGet]
         //(id alapján) – egy adott eredmény lekérdezése
         [HttpPost]
